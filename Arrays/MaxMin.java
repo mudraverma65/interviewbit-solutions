6816c310b78a7c3097015da7c070deee7866f114
Mudra Verma <47663461+mudraverma65@users.noreply.github.com>
2026-06-12 22:41:40 -0400

    Implement method to find min and max in array

---
+++ Arrays/MaxMin.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int solve(int[] A) {
+        int min = A[0];
+        int max = A[0];
+        
+        for(int i = 0; i<A.length; i++){
+            if(A[i] < min){
+                min = A[i];
+            }
+            if(A[i]> max){
+                max = A[i];
+            }
+        }
+        return min + max;
+    }
+}
